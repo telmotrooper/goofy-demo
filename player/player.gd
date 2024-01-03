34a0550b58e8580e3627423bba250d40ddbcbaa5
Telmo Valverde <telmo.valverde@tarmac.io>
2024-01-03 19:36:05 -0300

    Working on collisions

--- player/player.gd
+++ player/player.gd
@@ -7,7 +7,18 @@ func _physics_process(_delta: float) -> void:
 	velocity.y = Input.get_axis("ui_up", "ui_down") * SPEED
 
 	move_and_slide()
+	
+	var collision_data = get_collision_data()
+	
+	if collision_data.size() > 0 and Input.is_action_just_pressed("ui_select"):
+		var moving_block = collision_data[1]
+		moving_block.kick(collision_data[0])
 
-	if $Area2D.get_overlapping_bodies().size() > 0 and Input.is_action_just_pressed("ui_select"):
-		var moving_block = $Area2D.get_overlapping_bodies()[0]
-		moving_block.kick("right")
+func get_collision_data() -> Array:
+	if $AreaDown.get_overlapping_bodies().size() > 0:
+		return ["down", $AreaDown.get_overlapping_bodies()[0]]
+	if $AreaLeft.get_overlapping_bodies().size() > 0:
+		return ["left", $AreaLeft.get_overlapping_bodies()[0]]
+	if $AreaRight.get_overlapping_bodies().size() > 0:
+		return ["right", $AreaRight.get_overlapping_bodies()[0]]
+	return []
